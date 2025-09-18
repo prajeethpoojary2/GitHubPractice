d1ab6d6b293d58221c285ef24774421786130094
“yukthaks_lab” <yuktha.ks@codecraft.co.in>
2025-09-18 15:22:21 +0530

    Modified index.ts file by creating sum and product function

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+console.log("Hello");
+
+
+
+function sum(num1: number, num2: number): number{
+    let res = num1+num2;
+    return res;
+}
+
+function product(num1: number, num2: number): number{
+    let res = num1*num2;
+    return res
+}
