f0de0063f14775bde12a928147cb0c1f53bce92a
Shreesha Acharya <shreesha.acharya@codecraft.co.in>
2025-09-18 15:19:47 +0530

    Create a object

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
 console.log("Hello via Bun!");
+
+// My TS Object
+
+interface Mobile {
+    brand: string;
+    model: string; 
+    weight?: number;// optional
+    color: 'black' | 'grey' | 'while'; // unions
+    flashOn(): string;
+    flashOff(): string;
+}
+
+const myMobile:Mobile = {
+    brand: 'Samsung',
+    color: "grey",
+    model: "A 22",
+    flashOn() {
+        return "Mobile flash is On!";
+    },
+    flashOff() {
+        return "Mobile flash is Off!";
+    },
+};
